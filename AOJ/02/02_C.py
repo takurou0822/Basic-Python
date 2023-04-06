d712c5f10529f9773cddab32026190ac4eaee6c5
unknown <t.hiura.23.1.760@4nonome.com>
2023-04-06 15:16:03 +0900

    test21

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,8 +1,4 @@
-a,b,c = map (int, input().split())
-if a > b:
-    a,b = b,a
-if b > c:
-    b,c = c,b
-if a > b:
-    a,b = b,a
-print(a,b,c)
+a = list(map(int, input().split()))
+a.sort()
+print (*a)
+
