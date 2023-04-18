da27638e520db8473af9fe79b9ea103c41a0b7a8
unknown <t.hiura.23.1.760@4nonome.com>
2023-04-18 15:36:37 +0900

    test8-2

--- AOJ/08/08_C.py
+++ AOJ/08/08_C.py
@@ -2,12 +2,8 @@ import string
 import sys
 
 
-def alfa():     
-    return list(string.ascii_lowercase)
-
-
-def Alfa():
-    return list(string.ascii_uppercase)
+alfa = string.ascii_lowercase
+Alfa = string.ascii_uppercase
 
 
 atai = [0]*26
@@ -16,8 +12,8 @@ eigo_morau = sys.stdin.read()
 
 for i in eigo_morau:
     for m in range(26):
-        if i == alfa()[m] or i == Alfa()[m]:
+        if i == alfa[m] or i == Alfa[m]:
             atai[m] += 1
 
 for z in range(26):
-    print("{} : {}".format(alfa()[z], atai[z]))
+    print("{} : {}".format(alfa[z], atai[z]))
